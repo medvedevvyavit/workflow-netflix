5a914bd3290349a228dfc2c4cd9b86fb07943ddc
вячеслав медведев <medvedev.vv@reliab.tech>
2023-03-10 15:08:42 +0300

    initial-commit

---
+++ src/main/java/med/example/workflownetflix/WorkflowNetflixApplication.java
@@ -0,0 +1,13 @@
+package med.example.workflownetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowNetflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowNetflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/med/example/workflownetflix/WorkflowNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package med.example.workflownetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowNetflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
